ae60088fa95e21e342e79bf9107fb3ecd88bc3df
Alexis Langlet <alexislanglet34@gmail.com>
2024-05-18 13:20:38 +0200

    feat: add first http route

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # temperature-aggregator
 An API that aggregate temperature metrics, compute them and allow you to take an insight on temperature from all over the world
+
+This project is mainly a way to learn the different way of testing in Rust
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
-fn main() {
-    println!("Hello, world!");
-    
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app().into_make_service())
+        .await
+        .unwrap();
 }
 
+fn app() -> Router {
+    Router::new().route("/", get(hello_world))
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+
+    use tower::ServiceExt; // for `oneshot` and `ready`
+
+    #[tokio::test]
+    async fn hello_world() {
+        let app = app();
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
+}
